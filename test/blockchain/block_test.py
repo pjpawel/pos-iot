24cbd7fa0b0b79ed794a4a4c41b01a4b121ada85
pjpawel <pawel.jan.podgorski@gmail.com>
2023-08-09 19:36:53 +0200

    Add block test

---
+++ test/blockchain/block_test.py
@@ -0,0 +1,31 @@
+from io import BytesIO
+from time import time
+from uuid import uuid4
+from hashlib import sha256
+from copy import copy
+from pos.blockchain.transaction import Transaction
+from pos.blockchain.block import Block
+
+
+def test_encode_and_decode():
+    uid = uuid4()
+    signature = sha256(b'abc')
+    tx = Transaction(1, int(time()), uid, signature.digest(), {"message": "abc", "id": 5})
+    tx2 = copy(tx)
+    tx2.signature = sha256(b'def').digest()
+    tx2.data = {"message": "def", "id": 6}
+
+    block = Block(
+        2,
+        int(time()),
+        sha256(b'12345').digest(),
+        uuid4(),
+        sha256(b'asdfg').digest(),
+        [tx, tx2]
+    )
+
+    encoded_block = block.encode()
+
+    b_io = BytesIO(encoded_block)
+    block_new = Block.decode(b_io)
+    assert block_new == block
