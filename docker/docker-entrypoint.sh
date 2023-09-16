f42f5ed9eb7d9b3d9c4ee887d8fd1038b40821b5
pjpawel <pawel.jan.podgorski@gmail.com>
2023-09-16 14:59:58 +0200

    Update docker configuration according to new start scripts

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Script starting background jobs and flask app
+
+python3 start_scenario_job.py &
+python3 start_transaction_verifier_job.py &
+
+python3 -m flask --app pos/start_node_flask run --host=0.0.0.0
