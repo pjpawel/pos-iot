ca75a852642d6b353fbacabd2e4f56dde259651f
pjpawel <pawel.jan.podgorski@gmail.com>
2023-09-27 18:13:39 +0200

    add prefix to logger

--- pos/utils.py
+++ pos/utils.py
@@ -3,12 +3,12 @@ import os
 from logging.handlers import TimedRotatingFileHandler
 
 
-def setup_logger() -> None:
+def setup_logger(prefix: str = "") -> None:
     handler = TimedRotatingFileHandler(os.path.join(os.getenv('LOG_DIR', "log"), "app.log"), when="midnight")
     handler.suffix = "%Y%m%d"
     level = logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO'))
     logging.basicConfig(
-        format="%(asctime)s %(created)f %(levelname)s :::%(filename)s.%(module)s.%(lineno)d: %(message)s",
+        format=f"%(asctime)s-%(levelname)s:::%(filename)s.%(module)s.%(lineno)d:{prefix}: %(message)s",
         level=level,
         handlers=[handler],
         encoding="UTF-8",
