4ac11193f53ea1faad36eb8409416a50d66144ca
pjpawel <pawel.jan.podgorski@gmail.com>
2024-08-18 20:02:56 +0200

    Budowanie obrazu dokerowego za pomocą buildx

--- compose-start.sh
+++ compose-start.sh
@@ -8,7 +8,7 @@ NODE_NUMBER=$((6 + 11))
 
 # docker compose up --build
 
-docker build -t pot-iot .
+docker buildx build --no-cache -t pot-iot .
 
 docker run -d \
   --name node_genesis \
@@ -30,11 +30,11 @@ for ((i = 11 ; i < NODE_NUMBER ; i++)); do
     --name "node_normal_$i" \
     -p "50$i:5000" \
     --mount type=bind,source=/home/pp/programs/magisterka/storage,dst=/mnt/storage \
-      --env "SECRET_KEY=07f83cd2a279a2edc6b7ae1ced2ced8b77610efb2d2194813d7b446155d0b5$i" \
-      --env NODE_TYPE=SENSOR \
-      --env GENESIS_NODE=node_genesis \
-      --network pos-iot_blockchain \
-      pot-iot
+    --env "SECRET_KEY=07f83cd2a279a2edc6b7ae1ced2ced8b77610efb2d2194813d7b446155d0b5$i" \
+    --env NODE_TYPE=SENSOR \
+    --env GENESIS_NODE=node_genesis \
+    --network pos-iot_blockchain \
+    pot-iot
 done
 
 #
