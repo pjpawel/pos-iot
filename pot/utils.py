4d5b2f745732cc8afc36f7497b0f40fd677799f3
pjpawel <pawel.jan.podgorski@gmail.com>
2025-04-15 22:59:54 +0200

    Add additional test scenarios

--- pot/utils.py
+++ pot/utils.py
@@ -34,5 +34,46 @@ def prepare_simulation_env():
         case 3:
             if randint(0, 100) % 4 == 0:
                 os.environ["POT_SCENARIOS"] = "MAD_SENDER"
+        case 4:
+            # 50%
+            if randint(0, 100) % 2 == 0:
+                os.environ["POT_SCENARIOS"] = "MAD_SENDER"
+        case 5:
+            # 75%
+            if randint(0, 100) % 4 != 0:
+                os.environ["POT_SCENARIOS"] = "MAD_SENDER"
+        case 6:
+            os.environ["POT_SCENARIOS"] = "MAD_SENDER"
+        case 7:
+            os.environ["MAX_DELAY"] = "50"
+        case 8:
+            os.environ["MAX_DELAY"] = "100"
+            os.environ["MIN_DELAY"] = "50"
+        case 9:
+            os.environ["MAX_DELAY"] = "150"
+            os.environ["MIN_DELAY"] = "100"
+        case 10:
+            os.environ["MAX_DELAY"] = "200"
+            os.environ["MIN_DELAY"] = "150"
+        case 11:
+            os.environ["MAX_DELAY"] = "250"
+            os.environ["MIN_DELAY"] = "200"
+        case 12:
+            os.environ["VALIDATORS_PART"] = "0.1"
+        case 13:
+            os.environ["VALIDATORS_PART"] = "0.3"
+        case 14:
+            os.environ["VALIDATORS_PART"] = "0.4"
+        case 15:
+            os.environ["VALIDATORS_PART"] = "0.5"
+        case 16:
+            os.environ["VALIDATORS_PART"] = "0.6"
+        case 17:
+            os.environ["VALIDATORS_PART"] = "0.7"
+        case 18:
+            os.environ["VALIDATORS_PART"] = "0.8"
+        case 19:
+            os.environ["VALIDATORS_PART"] = "0.9"
         case _:
             os.environ["POT_SCENARIOS"] = "INSTANT_SENDER"
+            os.environ["VALIDATORS_PART"] = "0.2"
